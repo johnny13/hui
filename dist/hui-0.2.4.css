6eb270bc422657c099a7d07c3b4aaa679576a90c
Derek <derek@huement.com>
2013-11-28 23:04:01 -0600

    happy thanksgiving!

--- dist/hui-0.2.4.css
+++ dist/hui-0.2.4.css
@@ -1,4 +1,4 @@
-/*! huement user interface - v0.2.4 - 2013-11-25
+/*! huement user interface - v0.2.4 - 2013-11-28
 * http://hui.huement.com
 * Copyright (c) 2013 Derek Scott; Licensed MIT, GPLv3 */
 
