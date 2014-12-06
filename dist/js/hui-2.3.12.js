b0578d82cfb0695c4e5a23753097c8053c6b17a1
Derek Scott <dscott@freshlime.com>
2014-12-06 14:32:02 -0600

    Release v2.3.12

--- dist/js/hui-2.3.12.js
+++ dist/js/hui-2.3.12.js
@@ -1,4 +1,4 @@
-/*! huement user interface - v2.3.11 - 2014-12-06\n* hui.huement.com
+/*! huement user interface - v2.3.12 - 2014-12-06\n* hui.huement.com
 * Copyright (c) 2014 Derek Scott; License: MIT */
  
 /*! -- Built with Love and Bootstrap 3.1.1 -- */
