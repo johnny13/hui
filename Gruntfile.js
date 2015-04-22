caed81e4a022c64b97317909db35dadd495d8fd1
Derek <derek@huement.com>
2015-04-22 05:32:41 -0500

    Dependencies Fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -217,11 +217,9 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-recess');
-  grunt.loadNpmTasks('browserstack-runner');
   grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-template');
   grunt.loadNpmTasks('grunt-push-release');
